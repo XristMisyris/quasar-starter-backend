a85d1ad673125b8b305fb4991029bc1518df713d
Xristodoulos Misyris <xristmisyris@yahoo.gr>
2017-05-21 20:46:38 +0300

    no message

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::group(['middleware' => 'admin', 'prefix' => 'dashboard'], function () {
-    Route::get('/', 'HomeDashboardController@index')->name('dashboard.home');
-    Route::resource('parks', 'ParkController');
-    Route::resource('inverters', 'InverterController');
-});
